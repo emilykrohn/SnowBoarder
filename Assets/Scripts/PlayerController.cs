706b66d9f8c7219abccd93cc0b7b97a4ec84d0ea
Emily Krohn <emilykrohn2@gmail.com>
2024-06-08 07:37:37 -0700

    Disable Controls When Player Crashes
    Used a public method to allow the crash detector script to use the method from the player controller script.

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -9,6 +9,8 @@ public class PlayerController : MonoBehaviour
     [SerializeField] float baseSpeed = 25f;
     Rigidbody2D rb2d;
     SurfaceEffector2D surfaceEffector2D;
+    bool canMove = true;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -19,8 +21,16 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        RotatePlayer();
-        RespondToBoost();
+        if (canMove)
+        {
+            RotatePlayer();
+            RespondToBoost();
+        }
+    }
+
+    public void DisableControls()
+    {
+        canMove = false;
     }
     
     void RespondToBoost()
